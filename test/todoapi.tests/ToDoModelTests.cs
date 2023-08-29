31fd7a139e7cda9f5f4a1db6066fc3f0b866aa49
Kevin Wong <kevinwong202101@gmail.com>
2023-08-29 15:11:32 +1000

    add unit tests

---
+++ test/todoapi.tests/ToDoModelTests.cs
@@ -0,0 +1,17 @@
+using todoapi.Models;
+
+namespace todoapi.tests;
+
+public class ToDoModelTests
+{
+    [Fact]
+    public void CanChangeName()
+    {
+        var todoItem = new ToDoItem();
+        todoItem.Name = "complete unit test";
+
+        todoItem.Name="azure pipeline";
+
+        Assert.Equal("azure pipeline", todoItemgi.Name);
+    }
+}
